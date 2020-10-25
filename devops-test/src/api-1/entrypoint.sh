5a328f1b90c9b359634bc574e9cb95a1d8c1cf67
Atahualpa Pena <atahualpapena04@gmail.com>
2020-10-25 11:44:44 -0400

    latest changes

--- devops-test/src/api-1/entrypoint.sh
+++ devops-test/src/api-1/entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+
+
+until nc -z -v -w30 $DB_HOST 3306
+do
+  echo "Waiting for database connection..."
+  # wait for 5 seconds before check again
+  sleep 5
+done
+
 php artisan migrate 
 php artisan db:seed
-php artisan serve --port=80
+php artisan serve --port=9000
